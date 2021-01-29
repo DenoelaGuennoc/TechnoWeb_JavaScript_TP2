f3d96a1623b02967dc2676f1674b5ff077ec3bfa
Denoëla Guennoc <49162081+DenoelaGuennoc@users.noreply.github.com>
2021-01-29 17:24:49 +0100

    exo 1 et 2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Convertsseur €/franc
 let cible1 = document.getElementById("versfrancs");
 cible1.addEventListener("click", versfrancs);
 
@@ -14,4 +15,30 @@ function verseuros(event) {
   let entree = document.getElementById("valeur").value;
   entree = entree / 6.56;
   document.getElementById("resultat").value = entree.toFixed(2) + " euros";
+}
+
+//Mot de passe
+let cible = document.getElementById("passwd");
+cible.addEventListener("input", action);
+function action(event) {
+  let mdp = document.getElementById("passwd").value;
+  let long = mdp.length;
+  let message = "";
+  if (long === 0) {
+    message = "";
+  } else {
+    if (long <= 3) {
+      message = "longueur faible";
+      document.getElementById("msgPasswd").style.color = "red";
+    } else {
+      if (long < 6) {
+        message = "longueur moyenne";
+        document.getElementById("msgPasswd").style.color = "orange";
+      } else {
+        message = "longueur bonne";
+        document.getElementById("msgPasswd").style.color = "green";
+      }
+    }
+  }
+  document.getElementById("msgPasswd").textContent = message;
 }
